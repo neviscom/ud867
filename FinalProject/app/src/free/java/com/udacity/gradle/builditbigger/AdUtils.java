c10703dcddcaa77690a690b49b4a4d6f7bd41309
nikita.simonov <nikita.simonov@e-legion.com>
2016-10-10 16:40:52 +0300

    Step 5: Add a Paid Flavor. Remove the ad from the paid flavor

---
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/AdUtils.java
@@ -0,0 +1,24 @@
+package com.udacity.gradle.builditbigger;
+
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * @author Nikita Simonov
+ */
+
+public final class AdUtils {
+
+    public static void loadAd(View root) {
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+}
