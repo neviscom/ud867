126a8f667a1c5a7fbd79b7a4c25d1fdc3df7c03c
nikita.simonov <nikita.simonov@e-legion.com>
2016-10-10 16:31:55 +0300

    Step 5: Add a Paid Flavor. Remove the ad from the paid flavor

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -7,8 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.nevis.nanodegree.displayjokes.DisplayJokeActivity;
 
 
@@ -37,14 +35,7 @@ public class MainActivityFragment extends Fragment implements EndpointsAsyncTask
             }
         });
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+        AdUtils.loadAd(root);
         return root;
     }
 
---
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/AdUtils.java
@@ -0,0 +1,14 @@
+package com.udacity.gradle.builditbigger;
+
+import android.view.View;
+
+/**
+ * @author Nikita Simonov
+ */
+
+public final class AdUtils {
+
+    public static void loadAd(View root) {
+        // do nothing. Paid version don't have advertisement
+    }
+}
